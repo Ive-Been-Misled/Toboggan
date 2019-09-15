e2329aac02e85e341347ea5a6b3ab852596023ab
Em Zhan <zqianem@gmail.com>
2019-09-15 10:23:54 -0400

    Add doctype preambles to html files
    So firefox doesn't highlight stuff in red when viewing page source

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ import textwrap
 class Toboggan:
     @cherrypy.expose
     def index(self):
-        return textwrap.dedent("""
+        return textwrap.dedent("""\
+            <!DOCTYPE html>
             <h1>Hello World!</h1>
 
             <p>This is Toboggan, a text-based game generator.</p>
-
             <p>Access the gui by clicking <a href="gui">here</a>.</p>
         """)
 

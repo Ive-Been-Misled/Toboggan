114965a663a0f0078f04bfb029c635589cffbc99
Tom Paoloni <paoloni.6@buckeyemail.osu.edu>
2019-09-19 13:42:02 -0400

    Added game components; Testing with some test code

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import cherrypy
 import textwrap
+import game_components as gc
 
 class Toboggan:
     @cherrypy.expose
@@ -20,4 +21,20 @@ class Toboggan:
 
 cherrypy.tree.mount(Toboggan(), "/", "server.ini")
 cherrypy.engine.start()
+
+# Test Code
+g = gc.Game()
+
+print(g.player.current_room.title)
+print(g.player.current_room.characters)
+moved = g.player.move_to(g.player.current_room.north_room)
+print(moved)
+print(g.player.current_room.title)
+
+goblin = g.player.current_room.characters['Goblin']
+print(goblin.hit_points)
+g.player.attack(goblin, 20)
+print(goblin.hit_points)
+# Test Code
+
 cherrypy.engine.block()

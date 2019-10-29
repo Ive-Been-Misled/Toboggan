a2f4b7d240cd8ec804813c0b4f1aa8aa576d1f19
Em Zhan <zqianem@gmail.com>
2019-10-29 15:19:31 -0400

    Fix pylint issues with text_generators.py

--- toboggan/text_generators.py
+++ toboggan/text_generators.py
@@ -1,32 +1,34 @@
+"""Stateless NLP utility functions"""
 from os import environ
+import spacy
 
 if 'FAKE_GPT2' in environ:
     from .gpt2_fake import GPT2
 else:
     from .gpt2 import GPT2
 
-import spacy
 
 def describe_location(location: str) -> str:
+    """Given a location, returns a description of location"""
     prompt = f"""\
     You are in {location}. You look around. You see
     """
     description = 'You see'
 
     current_length = 0
-    min_length =  42
+    min_length = 42
 
     for token in GPT2.sample_sequence(prompt):
         description += token
         current_length += 1
         print(f"DEBUG: {token}")
-        if current_length >= min_length and (token == '.' or token == '?' or
-                token == '#' or token == '"'):
+        if current_length >= min_length and token in ('.', '?', '#', '"'):
             break
 
     return description
 
 def room_title_generator(text: str) -> list:
+    """Given a description, return a list of mentioned locations"""
     nlp = spacy.load('en_core_web_sm')
     doc = nlp(text)
     title_list = []

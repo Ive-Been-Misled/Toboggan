f43088308774766c9b9ed97ed007c12e2954bf87
Greshbolt <greshbolt@gmail.com>
2019-11-07 16:17:01 -0500

    Fixed error where None type was returned.

--- toboggan/noun_key.py
+++ toboggan/noun_key.py
@@ -3,10 +3,11 @@ class NounKey(Enum):
     LOCATIONS = ['place']
     CHARACTERS = ['character']
     ITEMS = ['object']
+    MISC = ['']
 
     @classmethod
     def find_name(cls, noun: str):
         for noun_class in NounKey:
             if noun in noun_class.value:
                 return noun_class
-        return None
+        return NounKey.MISC

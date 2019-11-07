638f644663a940a9d9a7ac6257605d779d6ea437
Em Zhan <zqianem@gmail.com>
2019-11-07 14:20:26 -0500

    Pre-load spacy model from text_generators
    instead of creating separate one

--- toboggan/watson_action_mapper.py
+++ toboggan/watson_action_mapper.py
@@ -4,8 +4,8 @@ import json
 from os import environ, path
 from datetime import datetime
 from ibm_watson import AssistantV1
-import spacy
 from .actions import Move, Percieve
+from .text_generators import _NLP
 
 
 class ActionMapper:
@@ -21,7 +21,7 @@ class ActionMapper:
             self._workspace_id = ActionMapper.create_workspace(self._assistant)
             ActionMapper.save_api_and_workspace_info(api_key, self._workspace_id)
 
-        self._nlp = spacy.load('en_core_web_lg')
+        self._nlp = _NLP
 
     def map(self, input_string):
         if input_string == 'look around':
